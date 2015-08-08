3c6b0854b37ae809d9cd7ddeae7d6ad743bf406a
jeffsee55 <jeffsee.55@gmail.com>
2015-08-08 10:31:39 -0600

    Added favicon

--- site/web/app/themes/sage/templates/head.php
+++ site/web/app/themes/sage/templates/head.php
@@ -2,6 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon"
+        type="image/png"
+        href="/app/themes/sage/dist/images/favicon.png">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
         rel="stylesheet">
   <?php wp_head(); ?>
