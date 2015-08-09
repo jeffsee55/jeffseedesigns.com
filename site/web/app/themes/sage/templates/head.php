8e37fa2fae9b9b3455ff0f7d685e31ca82714b12
jeffsee55 <jeffsee.55@gmail.com>
2015-08-08 19:24:23 -0600

    Fix favicon

--- site/web/app/themes/sage/templates/head.php
+++ site/web/app/themes/sage/templates/head.php
@@ -2,9 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon"
-        type="image/png"
-        href="/app/themes/sage/dist/images/favicon.png">
+  <link rel="icon" type="image/png" href="/app/themes/sage/dist/images/favicon.png">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
         rel="stylesheet">
   <?php wp_head(); ?>
